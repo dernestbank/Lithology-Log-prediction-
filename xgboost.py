10ec70fccde9f380d664a0e42a3fa9e1159f428f
dernestbank <dernestbank@yahoo.com>
2021-05-11 12:28:06 +0000

    i've added a few optimizations and I've started an xgboast learning

--- xgboost.py
+++
@@ -1,8 +0,0 @@
-from xgboost import XGBRegressor
-from sklearn.datasets import load_boston
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import cross_val_score
-from sklearn.metrics import mean_squared_error
-from matplotlib import pyplot as plt
-
-
